7909d479e4b4f80e3b217e1d632fde8de7aa6997
Jan Bartunek <gsus0187@gmail.com>
2015-01-07 15:16:43 +0200

    updated README.md and fixed few issues in gulpfile.js

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # microsite-seed
 
+Based on [HTML5Boilerplate](http://html5boilerplate.com/), using [Gulp](http://gulpjs.com/) and [LESS](http://lesscss.org/)
+
+Sometimes you need just quickly develop some static site, eg. some proof-of-concept for your boss, client or colleague. Or marketing microsite. Or simple site for your mum. You want to benefit from tools you're used to, but don't want to spend time with setting things up. Just start write code.
+
+## Starting is simple:
+- clone this repo
+- run `npm install`
+- run `gulp`
+- write some code...
+- check your work on `//localhost:4242`
+- enable [Live Reload](http://livereload.com/) in your browser for speeding up development
+
+## How it works?
+
+Simply. Gulp script will take care of compilation of your less files, running connect server and serving your files. Connect server will server all files from `_public` folder.
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// Import all necessary modules
 var gulp = require('gulp'),
     less = require('gulp-less'),
     prefix = require('gulp-autoprefixer'),
@@ -5,20 +6,34 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     del = require('del'),
 
-    LESS = 'code/less/**/*.less',
+    // Chose port for runnig dev server on
+    DEV_PORT = 4242,
+
+    // Where are my LESS files and main less file name
+    LESS_FOLDER =     'code/less/',
+    LESS_FILES =      LESS_FOLDER + '**/*.less',
+    LESS_MAIN_FILE =  LESS_FOLDER + 'main.less',
+
+    // What to copy to public folder
+    // to be distributed by dev server
     FILES = [ 'code/**/*.*', '!code/less/**/*'],
+
+    // Name of public folder
     DIST_FOLDER = '_public/';
 
+// Clean completely public folder
 gulp.task('clean', function (cb) {
-  del( '_public/*', {force: true}, cb );
+  del( DIST_FOLDER + '*', {force: true}, cb );
 });
 
+// Copy ALL files to public folder
 gulp.task('copy', ['clean'], function () {
   return gulp.src( FILES )
     .pipe( gulp.dest( DIST_FOLDER ) )
     .pipe( connect.reload() );
 });
 
+// Copy JUST CHANGED files to public folder
 gulp.task('copy:changed', function () {
   return gulp.src( FILES )
     .pipe( changed( DIST_FOLDER  ) )
@@ -29,29 +44,37 @@ gulp.task('copy:changed', function () {
 // Start server with livereload enabled
 gulp.task('connect', function () {
   connect.server({
-    root: ['_public'],
-    port: 4242,
+    root: [ DIST_FOLDER ],
+    port: DEV_PORT,
     livereload: true
   });
 });
 
 // Compile less files
 gulp.task('less', function () {
-  return gulp.src( 'code/less/main.less' )
+  return gulp.src( LESS_MAIN_FILE )
     .pipe( less() )
     .pipe( prefix() )
-    .pipe( gulp.dest('_public/css/') )
+    .pipe( gulp.dest( DIST_FOLDER + 'css/') )
     .pipe( connect.reload() );
 });
 
-// Default entry point, compile, start watchers and dev server
-gulp.task('default', ['copy', 'less', 'connect'], function () {
-  gulp.watch( FILES, ['copy:changed'] );
-  gulp.watch( LESS, ['less'] );
-  console.log('Watching files...');
+// Compile less files
+gulp.task('less:build', ['copy'], function () {
+  return gulp.src( LESS_MAIN_FILE )
+    .pipe( less() )
+    .pipe( prefix() )
+    .pipe( gulp.dest( DIST_FOLDER + 'css/') );
 });
 
 // Just build and compile everything and don't start the server and watchers
-gulp.task('build', ['copy', 'less'], function () {
+gulp.task('build', ['less:build'], function () {
   console.log('Build finished');
+});
+
+// Default entry point, compile, start watchers and dev server
+gulp.task('default', ['build', 'connect'], function () {
+  gulp.watch( FILES, ['copy:changed'] );
+  gulp.watch( LESS_FILES, ['less'] );
+  console.log('Watching files...');
 });
