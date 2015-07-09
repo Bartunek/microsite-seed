ec096b60954b8189157f637cc943751495391c27
Jan Bartunek <gsus0187@gmail.com>
2015-07-09 16:20:05 +0200

    Added support for Jade templates and Stylus CSS preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Import all necessary modules
 var gulp =        require('gulp'),
     less =        require('gulp-less'),
+    stylus =      require('gulp-stylus'),
     prefix =      require('gulp-autoprefixer'),
     changed =     require('gulp-changed'),
     connect =     require('gulp-connect'),
@@ -10,6 +11,7 @@ var gulp =        require('gulp'),
     concat =      require('gulp-concat'),
     sourcemaps =  require('gulp-sourcemaps'),
     babel =       require('gulp-babel'),
+    jade =        require('gulp-jade'),
     bowerFiles =  require('main-bower-files'),
     del =         require('del'),
     util =        require('util'),
@@ -19,20 +21,29 @@ var gulp =        require('gulp'),
     DEV_PORT = 3000,
 
     // Where are my LESS files and main less file name
-    LESS_FOLDER =     'code/less/',
-    LESS_FILES =      LESS_FOLDER + '**/*.less',
-    LESS_MAIN_FILE =  LESS_FOLDER + 'main.less',
+    LESS_FOLDER =       'code/less/',
+    LESS_FILES =        LESS_FOLDER + '**/*.less',
+    LESS_MAIN_FILE =    LESS_FOLDER + 'main.less',
+
+    STYLUS_FOLDER =     'code/styl/',
+    STYLUS_FILES =      STYLUS_FOLDER + '**/*.styl',
+    STYLUS_MAIN_FILE =  STYLUS_FOLDER + 'main.stylus.styl',
 
     // What to copy to public folder
     // to be distributed by dev server
     FILES = [
       'code/**/*.*',
+      '!code/**/*.jade',
       '!code/js/**/*',
-      '!code/less/**/*'
+      '!code/less/**/*',
+      '!code/styl/**/*'
     ],
 
     // Javascript and JSX files, transpiled by Babel
     JS_FILES = 'code/js/**/*.{js,jsx}',
+
+    // Jade Files to be transformed to HTML
+    JADE_FILES = 'code/**/*.jade',
 
     // Name of public folder
     DIST_FOLDER = '_public/';
@@ -90,6 +101,27 @@ gulp.task('browsersync', function () {
   });
 });
 
+// Compile jade files
+gulp.task('jade', function () {
+  return gulp.src( JADE_FILES )
+    .pipe( jade({
+      pretty: true
+    }) )
+    .pipe( gulp.dest( DIST_FOLDER ) );
+});
+
+// Compile stylus files
+gulp.task('stylus', function () {
+  return gulp.src( STYLUS_FILES )
+    .pipe( plumber( { errorHandler: onError } ) )
+    .pipe( stylus({
+      compress: true
+    }) )
+    .pipe( prefix() )
+    .pipe( gulp.dest( DIST_FOLDER + 'css/' ) )
+    .pipe( browserSync.stream() );
+});
+
 // Compile less files
 gulp.task('less', function () {
   return gulp.src( LESS_MAIN_FILE )
@@ -111,7 +143,7 @@ gulp.task('babel', function () {
 });
 
 // Just build and compile everything and don't start the server and watchers
-gulp.task('build', ['copy:vendor', 'babel'], function () {
+gulp.task('build', ['copy:vendor', 'babel', 'jade', 'stylus'], function () {
   console.log('Building files...');
   return gulp.src( LESS_MAIN_FILE )
     .pipe( less() )
@@ -121,8 +153,10 @@ gulp.task('build', ['copy:vendor', 'babel'], function () {
 
 // Default entry point, compile, start watchers and dev server
 gulp.task('default', ['build', 'connect', 'browsersync'], function () {
-  gulp.watch( FILES, ['copy:changed'] ).on( 'change', browserSync.reload );
-  gulp.watch( LESS_FILES, ['less'] );
-  gulp.watch( JS_FILES, ['babel'] ).on( 'change', browserSync.reload );
+  gulp.watch( FILES,        ['copy:changed'] ).on( 'change', browserSync.reload );
+  gulp.watch( LESS_FILES,   ['less'] );
+  gulp.watch( STYLUS_FILES, ['stylus'] );
+  gulp.watch( JS_FILES,     ['babel'] ).on( 'change', browserSync.reload );
+  gulp.watch( JADE_FILES,   ['jade'] ).on( 'change', browserSync.reload );
   console.log('Watching files...');
 });
